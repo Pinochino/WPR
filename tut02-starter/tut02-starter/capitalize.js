33c2ae65c7598490c9f91d0470616923340d6ddf
Pinochino <tranhunghp22112004@gmail.com>
2024-08-25 09:19:20 +0700

    Tutorial 01 + 02

---
+++ tut02-starter/tut02-starter/capitalize.js
@@ -0,0 +1,17 @@
+/*
+ * Capitalizing text of all paragraphs
+ */
+"use strict";
+(function() {
+
+  window.addEventListener("load", init);
+
+  /**
+   * init - write your logic here
+   */
+  function init() {
+    
+	
+  }
+
+})();
---
+++ tut02-starter/tut02-starter/equations/equations.js
@@ -0,0 +1,23 @@
+/*
+ * Quadratic equation solver exercise
+ *
+ * TODO: implement the functionality for index.html webpage to solve quadratic equations
+ * Use-case scenario: when a user enters numbers on three text fields with ids of "a",
+ * "b", "c" and clicks "Solve" button, equation solution will be displayed on the <div>
+ * with id "result".
+ * 
+ */
+ "use strict";
+(function() {
+
+  window.addEventListener("load", init);
+
+  /**
+   * init - write your logic here
+   */
+  function init() {
+    
+	
+  }
+
+})();
